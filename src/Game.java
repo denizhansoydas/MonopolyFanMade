afceb243aa54ee1fe12a641e8a0136b528b583d5
denizhansoydas <32513396+denizhansoydas@users.noreply.github.com>
2022-04-01 10:40:09 +0300

    Players are added.
    Default Constructor is simplified.

--- src/Game.java
+++ src/Game.java
@@ -2,11 +2,19 @@ import java.util.ArrayList;
 
 public class Game {
     ArrayList<Square> squares;
+    Die die1;
+    Die die2;
+    Player player1;
+    Player player2;
     Game(){
-        squares = new ArrayList<>();
+        this(new ArrayList<>());
     }
     Game(ArrayList<Square> squares){
         this.squares = squares;
+        die1 = new Die();
+        die2 = new Die();
+        player1 = new Player();
+        player2 = new Player();
     }
     public void appendSquare(Square square){
         squares.add(square);
