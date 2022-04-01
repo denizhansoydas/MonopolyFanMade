f669d09016fdcc5a0d6a9977ac5b364498c4ee5c
denizhansoydas <32513396+denizhansoydas@users.noreply.github.com>
2022-04-01 15:22:22 +0300

    Encapsulated.

--- src/Square.java
+++ src/Square.java
@@ -1,4 +1,33 @@
 public abstract class Square {
+    protected String name;
     private int cost;
 
+
+    Square(String name, int cost){
+        this.name = name;
+        this.cost = cost;
+    }
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "name='" + name + '\'' +
+                ", cost=" + cost +
+                '}';
+    }
 }
