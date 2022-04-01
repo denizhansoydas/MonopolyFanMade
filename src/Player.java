136a208b027db5d420221e75c9a6dc7d4ec6e943
denizhansoydas <32513396+denizhansoydas@users.noreply.github.com>
2022-04-01 15:26:15 +0300

    Core functionalities of users are introduced.

--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,30 @@
 public class Player extends User{
     public static final int INITIAL_MONEY = 15000;
-    Player(){
-        super(INITIAL_MONEY);
+    private int location; // start: 1
+    int jailCount;
+    Player(Game game){
+        super(game,INITIAL_MONEY);
+        location = 1;
+        jailCount = 0;
     }
+
+    public int getLocation() {
+        return location;
+    }
+
+    public void setLocation(int location) {
+        this.location = location;
+
+    }
+
+    public int getJailCount() {
+        return jailCount;
+    }
+
+    public void setJailCount(int jailCount) {
+        this.jailCount = jailCount;
+    }
+    //    public void performAction(int location){
+//
+//    }
 }
